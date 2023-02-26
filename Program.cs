3d9af436fdea73b6369f0610007ddee5229a9837
retusu <konstantinovworks@gmail.com>
2023-02-26 19:17:19 +0200

    App release 1.0

--- Program.cs
+++ Program.cs
@@ -2,9 +2,6 @@ namespace JapanRandomHiraganaKatakana
 {
     internal static class Program
     {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
